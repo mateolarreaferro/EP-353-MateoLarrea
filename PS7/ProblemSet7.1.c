811d6c25297254752189857a35438647224e778d
mateolarreaferro <55174368+mateolarreaferro@users.noreply.github.com>
2020-10-27 11:44:19 -0500

    PS7 Uploaded
    PS7.1 corrected

--- PS7/ProblemSet7.1.c
+++ PS7/ProblemSet7.1.c
@@ -5,45 +5,63 @@
 #include <stdbool.h>
 
 
-
-
-
-
-// Compute the average of all long integers entered by the user when they exit the number entering routine.
-// Print out the result of averaging.
-// Do not forget to free the memory before the program exits.
-// Hint: How do we change the size of an array on the fly?
-// Hint: You will also need to keep track of the number of times the users entered an integer.
-
-
-
-
-
-
 int main(){
 
-// The size of the array must always be the same as the number of long integers entered by the users.
-long int array [size];
-int size = 1; //Amount of numbers inputed by user(The size of the array must always be the same as the number of long integers entered by the user)
-long int inputs;
-int maxArrayLength = 100;
+	// The size of the array must always be the same as the number of long integers entered by the users.
+	long int *arr = malloc(sizeof(long int) * 100);
 
 
-// Write a program that takes an arbitrary number of long integers from users using scanf and while-loop.
 
-printf("Enter numbers that will be averaged: \n");
+	int size = 0; //Amount of numbers inputed by user(The size of the array must always be the same as the number of long integers entered by the user)
+	long int inputs;
+	int maxArrayLength = 100;
+
+	//Variables to Average
+	int sumofElements;
+	
 
 
-while(inputs != 0 || size < maxArrayLength) // The while-loop should exit if the users enter 0 or the array becomes larger than 100.
-{
-// Store all long integers entered by the users in an array while the while-loop is running.
-scanf("%li", &inputs);
-size += 1;
+	// Write a program that takes an arbitrary number of long integers from users using scanf and while-loop.
+
+	printf("Enter numbers that will be averaged: \n");
+
+
+	while(1) // The while-loop should exit if the users enter 0 or the array becomes larger than 100.
+	{
+	
+	// Store all long integers entered by the users in an array while the while-loop is running.
+	scanf("%li", &inputs);
+	size += 1;
+	arr[size] = inputs;
+	
+
+	if (inputs == 0 || size > maxArrayLength){
+
+		break; //maybe something else
+		}
+	}
+
+	
+
+	arr = realloc(arr, size * sizeof(long int));
+
+
+	// Compute the average of all long integers entered by the user when they exit the number entering routine.
+
+	for (int i = 0; i < size; i++){
+
+	sumofElements += arr[i];
 
 }
+	size = size - 1; //This ignores the 0 or the 100th number
+	float average = (float)sumofElements / size;
+
+	
+	// Print out the result of averaging.
+	printf("The average of all of the numbers entered in the array is %3.2f\n",average);
 
 
-	// free(x);
+	free(arr);
 	return 0;
   }
 
